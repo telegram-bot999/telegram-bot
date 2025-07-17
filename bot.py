cfcc78e7cb6deb7bd454a8688d8a58a75595fee5
telegram-bot999 <vram1977@gmail.com>
2025-07-17 19:22:25 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,18 @@
+import logging
+import os
+from telegram import Update
+from telegram.ext import ApplicationBuilder, ContextTypes, MessageHandler, filters
+
+logging.basicConfig(level=logging.INFO)
+
+TOKEN = os.getenv("TOKEN")
+
+async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    user_message = update.message.text
+    await update.message.reply_text(f"Ты сказал: {user_message}")
+
+if __name__ == "__main__":
+    app = ApplicationBuilder().token(TOKEN).build()
+    app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
+    app.run_polling()
 
